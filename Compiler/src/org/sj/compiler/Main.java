8bbe5923bd6e84c2ba05741dd6340176ad9f59ac
Stone <1592277253@qq.com>
2023-03-27 13:10:10 +0800

    build the framework of DevRegex and implement substitute user defined regex.

---
+++ Compiler/src/org/sj/compiler/Main.java
@@ -0,0 +1,9 @@
+package org.sj.compiler;
+
+public class Main {
+    public static void main(String[] args) {
+        Test.TestRegex();
+    }
+
+
+}
---
+++ Compiler/src/org/sj/compiler/lex/ExpressionNode.java
@@ -0,0 +1,7 @@
+package org.sj.compiler.lex;
+
+public class ExpressionNode {
+    boolean isLexRegexOperator;
+    LexRegexOperator operator;
+    String content;
+}
